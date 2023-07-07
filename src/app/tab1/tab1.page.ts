42f1fd91f20da4a036b5abbbd8758f21258372a7
Samuel Reina <sreina@procesosytecnologia.com>
2023-07-07 17:02:00 -0500

    Fin de la sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit(){
+
+    let num = -1; 
+
+    let num2 = -2; 
+    
+    let num3 = num + num2; 
+
+    if(num3 > 0 ){
+      console.log("numero"+num3)
+    }
+    else(
+      console.log("numero menor ")
+      
+    )
+
+    console.log(num3)
+  }
 
 }
